b0fd600f02f8349a4f91b84151041704a450d453
Kamil Chmielewski <kamil.chm@gmail.com>
2017-03-11 14:42:59 +0100

    Hello world solution

--- exercises/hello-world/hello_world.exs
+++ exercises/hello-world/hello_world.exs
@@ -18,14 +18,16 @@ defmodule HelloWorld do
   http://elixir-lang.org/getting-started/modules.html#default-arguments
   """
 
+  def hello() do
+    "Hello, World!"
+  end
+
   @doc """
   Greets the user by name, or by saying "Hello, World!"
   if no name is given.
   """
   @spec hello(String.t) :: String.t
   def hello(name) do
-
-    "Your implementation goes here"
-
+    "Hello, #{name}!"
   end
 end
